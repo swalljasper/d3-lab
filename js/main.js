7f790299c59b6e4136758a09700f378ea04930c8
SorenWalljasper <swalljasper@wisc.edu>
2016-04-05 22:57:26 -0500

    module 9 commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
+(function(){
+
+	var attrArray = ["frn_brn", "pvrty", "rent", "unemp", "unins"];
+	var expressed = attrArray[0];
+
+
 window.onload = setMap();
 
 function setMap(){
-	var width = 960,
+	var width = window.innerWidth * 0.5,
 		height = 560;
 
 	var map = d3.select("body")
@@ -29,66 +35,26 @@ function setMap(){
 
 	function callback(error, csvData, tracts, water){
 
-		// var graticule = d3.geo.graticule()
-		// 	.step([0.01, 0.01]);
-
-		// var gratBackground = map.append("path")
-		// 	.datum(graticule.outline())
-		// 	.attr("class", "gratBackground")
-		// 	.attr("d", path)
-
-		// var gratLines = map.selectAll(".gratLines")
-		// 	.data(graticule.lines())
-		// 	.enter()
-		// 	.append("path")
-		// 	.attr("class", "gratLines")
-		// 	.attr("d", path);
+		//setGraticule(map, path);
 
 		var hydro = topojson.feature(water, water.objects.Water),
             censusTracts = topojson.feature(tracts, tracts.objects.censusTracts).features;
 
-        var mplsCensusTracts = map.selectAll(".regions")
-        	.data(censusTracts)
-        	.enter()
-        	.append("path")
-        	.attr("class", function(d){
-        		return "mplsCensusTracts " + d.properties.adm1_code;
-        	})
-        	.attr("d", path);
+       
+        censusTracts = joinData(censusTracts, csvData)
+
+        var colorScale = makeColorScale(csvData);
+
+        setEnumerationUnits(censusTracts, map, path, colorScale);
+
+        setChart(csvData, colorScale);
 
         var lakesRivers = map.append("path")
         	.datum(hydro)
         	.attr("class", "lakesRivers")
         	.attr("d", path);
 
-        var attrArray = ["frn_brn", "pvrty", "rent", "unemp", "unins"];
-
-        for (var i=0; i<csvData.length; i++){
-        	var csvCT = csvData[i];
-        	var csvKey = csvCT.ID;
-
-        	for (var a=0; a<censusTracts.length; a++){
-
-        		var geojsonProps = censusTracts[a].properties;
-        		var geojsonKey = geojsonProps.GEOID;
-
-        		if (geojsonKey == csvKey){
-        			attrArray.forEach(function(attr){
-        				var val = parseFloat(csvCT[attr]);
-        				geojsonProps[attr] = val;
-        			});
-        		};
-        	};
-        };
-
-        var colorScale = makeColorScale(csvData);
-
-        setEnumerationUnits(censusTracts, map, path, colorScale);
-
-        
-
-        console.log(csvData)
-        console.log(censusTracts)
+ 
 		
 	}
 
@@ -96,11 +62,11 @@ function setMap(){
 
 function makeColorScale(data){
         	var colorClasses = [
-        		"#D4B9DA",
-        		"#C994C7",
-        		"#DF65B0",
-        		"#DD1C77",
-        		"#980043"
+        		"#eff3ff",
+        		"#bdd7e7",
+        		"#6baed6",
+        		"#3182bd",
+        		"#08519c"
     		];
 
     		var colorScale = d3.scale.quantile()
@@ -110,7 +76,6 @@ function makeColorScale(data){
 
     		var domainArray = [];
     		for (var i=0; i<data.length; i++){
-    			console.log(data[i]);
     			var val = parseFloat(data[i][expressed]);
     			domainArray.push(val);
     		}
@@ -134,18 +99,165 @@ function makeColorScale(data){
 
 function setEnumerationUnits(censusTracts, map, path, colorScale){
 
-	var cenTract = map.selectAll(".regions")
+	var zones = map.selectAll(".zones")
 		.data(censusTracts)
 		.enter()
 		.append("path")
 		.attr("class", function(d){
-			return "censusTracts " + d.properties.ID;
+			return "zones " + d.properties.GEOID;
 		})
 		.attr("d", path)
 		.style("fill", function(d){
-			return colorScale(d.properties[expressed]);
+			return choropleth(d.properties, colorScale);
 		});
 };
+
+function joinData(censusTracts, csvData){
+
+	for (var i=0; i<csvData.length; i++){
+        	var csvCT = csvData[i];
+        	var csvKey = csvCT.ID;
+
+        	for (var a=0; a<censusTracts.length; a++){
+
+        		var geojsonProps = censusTracts[a].properties;
+        		var geojsonKey = geojsonProps.GEOID;
+
+        		if (geojsonKey == csvKey){
+        			attrArray.forEach(function(attr){
+        				var val = parseFloat(csvCT[attr]);
+        				geojsonProps[attr] = val;
+        			});
+        		};
+        	};
+        };
+
+    return censusTracts; 
+
+};
+
+		
+function setGraticule(map, path){
+
+		var graticule = d3.geo.graticule()
+			.step([0.01, 0.01]);
+
+		var gratBackground = map.append("path")
+			.datum(graticule.outline())
+			.attr("class", "gratBackground")
+			.attr("d", path)
+
+		var gratLines = map.selectAll(".gratLines")
+			.data(graticule.lines())
+			.enter()
+			.append("path")
+			.attr("class", "gratLines")
+			.attr("d", path);
+};
+
+function choropleth(props, colorScale){
+	var val = parseFloat(props[expressed]);
+
+	if (val && val != NaN){
+		return colorScale(val);
+	} else {
+		return "#CCC"
+	};
+};
+
+function setChart(csvData, colorScale){
+	
+	var chartWidth = window.innerWidth * 0.4,
+		chartHeight = 560,
+		leftPadding = 25,
+		rightPadding = 2,
+		topBottemPadding = 5,
+		chartInnerWidth = chartWidth - leftPadding - rightPadding,
+		chartInnerHeight = chartHeight - topBottemPadding * 2,
+		translate = "translate(" + leftPadding + "," + topBottemPadding + ")";
+
+	var chart = d3.select("body")
+		.append("svg")
+		.attr("width", chartWidth)
+		.attr("height", chartHeight)
+		.attr("class", "chart");
+
+	var chartBackground = chart.append("rect")
+		.attr("class", "chartBackground")
+		.attr("width", "chartInnerWidth")
+		.attr("height", "chartInnerHeight")
+		.attr("transform", "translate");
+
+	var yScale = d3.scale.linear()
+		.range([0, chartHeight])
+		.domain([0, .5]);
+
+	var bars = chart.selectAll(".bars")
+		.data(csvData)
+		.enter()
+		.append("rect")
+		.sort(function(a, b){
+			return a[expressed]-b[expressed]
+		})
+		.attr("class", function(d){
+			return "bars " + d.ID;
+		})
+		.attr("width", chartWidth/ csvData.length - 1)
+		.attr("x", function(d, i){
+			return i * (chartWidth / csvData.length);
+		})
+		.attr("height", function(d){
+			return yScale(parseFloat(d[expressed]));
+		})
+		.attr("y", function(d){
+			return chartHeight - yScale(parseFloat(d[expressed]));
+		})
+		.style("fill", function(d){
+			return choropleth(d, colorScale);
+		});
+
+	var chartTitle = chart.append("text")
+		.attr("x", 20)
+		.attr("y", 40)
+		.attr("class", "chartTitle")
+		.text(expressed + " in each CensusTracts")
+
+	var yAxis = d3.svg.axis()
+		.scale(yScale)
+		.orient("left");
+
+	var axis = chart.append("g")
+		.attr("class", "axis")
+		.attr("transform", translate)
+		.call(yAxis)
+
+	// var xValue = function(d){
+	// 	console.log()
+	// 	return d.properties.frn_brn;
+	// 	}
+	// var	xScale = d3.scale.linear().range([0, chartWidth])
+	// var	xMap = function(d) {
+	// 		return xScale(xValue(d));
+	// 	}
+	// var	xAxis = d3.svg.axis().scale(xScale).orient("bottem")
+
+	// var yValue = function(d){
+	// 		return d.properties.pvrty;
+	// 	}
+	// var	yScale = d3.scale.linear().range([chartHeight, 0])
+	// var	yMap = function(d){
+	// 		return yScale(yValue(d));
+	// 	}
+	// var	yAxis = d3.svg.axis().scale(yScale).orient("left");
+
+	// var cValue = function(d) {
+	// 	return d.choropleth;
+	// 	}
+ };
+
+
+
+})();
 
 
 
